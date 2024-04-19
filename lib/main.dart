e960954fc3154060a895f65106ee3e5fe7138edc
felizardo arrabis jr. <arrabis.felizardo.veniegas@gmail.com>
2024-04-19 09:53:27 +0800

    necessary changes for onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sukify/view/onboardingScreen/onboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: Scaffold(),
+      home: const OnboardingScreen(),
     );
   }
 }
