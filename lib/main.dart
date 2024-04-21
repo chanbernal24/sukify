5b7c9fe3b60e44bcf0146d64e9787f66c63d6c06
felizardo arrabis jr. <arrabis.felizardo.veniegas@gmail.com>
2024-04-21 13:38:43 +0800

    used in testing

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const SignupScreen(),
+      home: const OnboardingScreen(),
     );
   }
 }
